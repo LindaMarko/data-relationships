368889b7eea437624f4d41fd130e5b530089e6d2
Linda Marko <lindamarko83@gmail.com>
2022-03-29 11:47:22 +0200

    Define Associations

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-'use strict';
+'use strict'
 
-const { sequelize, models } = require('./db');
+const { sequelize, models } = require('./db')
 
 // Get references to our models.
-const { Person, Movie } = models;
+const { Person, Movie } = models
 
 // Define variables for the people and movies.
 // NOTE: We'll use these variables to assist with the creation
 // of our related data after we've defined the relationships
 // (or associations) between our models.
-let bradBird;
-let vinDiesel;
-let eliMarienthal;
-let craigTNelson;
-let hollyHunter;
-let theIronGiant;
-let theIncredibles;
+let bradBird
+let vinDiesel
+let eliMarienthal
+let craigTNelson
+let hollyHunter
+let theIronGiant
+let theIncredibles
 
-console.log('Testing the connection to the database...');
+console.log('Testing the connection to the database...')
 
-(async () => {
+;(async () => {
   try {
     // Test the connection to the database
-    console.log('Connection to the database successful!');
+    await sequelize.authenticate()
+    console.log('Connection to the database successful!')
 
     // Sync the models
-    console.log('Synchronizing the models with the database...');
+    await sequelize.sync({ force: true })
+    console.log('Synchronizing the models with the database...')
 
     // Add People to the Database
-    console.log('Adding people to the database...');
-  
+    console.log('Adding people to the database...')
+
     // Update the global variables for the people instances
 
     // Add Movies to the Database
-    console.log('Adding movies to the database...');
+    console.log('Adding movies to the database...')
 
     // Retrieve movies
 
     // Retrieve people
 
-    process.exit();
+    process.exit()
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
-      const errors = error.errors.map(err => err.message);
-      console.error('Validation errors: ', errors);
+      const errors = error.errors.map((err) => err.message)
+      console.error('Validation errors: ', errors)
     } else {
-      throw error;
+      throw error
     }
   }
-})();
+})()
--- db/models/movie.js
+++ db/models/movie.js
@@ -1,27 +1,31 @@
-'use strict';
-const Sequelize = require('sequelize');
+'use strict'
+const Sequelize = require('sequelize')
 
 module.exports = (sequelize) => {
   class Movie extends Sequelize.Model {}
-  Movie.init({
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  Movie.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      releaseYear: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
     },
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    releaseYear: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-  }, { sequelize });
+    { sequelize }
+  )
 
   Movie.associate = (models) => {
     // TODO Add associations.
-  };
+    Movie.belongsTo(models.Person)
+  }
 
-  return Movie;
-};
+  return Movie
+}
